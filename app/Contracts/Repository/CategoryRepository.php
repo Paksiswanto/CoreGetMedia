8575e816a3f108367d75db5141e0662f89debff6
ArdianS18 <ardiansupriadi464@gmail.com>
2024-06-07 08:47:50 +0700

    interface dan repository

---
+++ app/Contracts/Repository/CategoryRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Contracts\Repositories;
+
+use App\Contracts\Interfaces\CategoryInterface;
+use App\Models\Category;
+
+class CategoryRepository extends BaseRepository implements CategoryInterface
+{
+    public function __construct(Category $category)
+    {
+        $this->model = $category;
+    }
+
+    /**
+     * Handle show method and delete data instantly from models.
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function delete(mixed $id): mixed
+    {
+        return $this->model->query()
+        ->findOrFail($id)
+        ->delete();
+    }
+
+    /**
+     * Handle get the specified data by id from models.
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function show(mixed $id): mixed
+    {
+        //
+    }
+
+    /**
+     * Handle the Get all data event from models.
+     *
+     * @return mixed
+     */
+    public function get(): mixed
+    {
+        return $this->model->query()
+            ->get();
+    }
+
+    /**
+     * Handle store data event to models.
+     *
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function store(array $data): mixed
+    {
+        return $this->model->query()
+            ->create($data);
+    }
+
+    /**
+     * Handle show method and update data instantly from models.
+     *
+     * @param mixed $id
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function update(mixed $id, array $data): mixed
+    {
+        return $this->model->query()
+            ->findOrFail($id)
+            ->update($data);
+    }
+}
