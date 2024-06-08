1985c489c578e3ff6fac60846474855f83021785
jovitamaharani <jovitamaharani321@gmail.com>
2024-06-08 18:13:24 +0700

    all news, iklan

--- routes/web.php
+++ routes/web.php
@@ -26,32 +26,24 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/category', function(){
-    return view('pages.user.category.index');
-})->name('news.category');
-
-Route::get('subcategory', function () {
-    return view('pages.user.subcategory.index');
-})->name('news.subcategory');
-
 Route::get('/dashboard', function () {
     return view('pages.admin.home.index');
-})->name('dashboard.admin');
-
-Route::get('faq-list', [FaqController::class, 'index'])->name('faq.list.admin');
-
-Route::get('tag-list', [TagsController::class, 'index'])->name('tag.list.admin');
-
-Route::get('category-list', [CategoryController::class, 'index'])->name('category.list.admin');
-
+    })->name('dashboard.admin');
+    
+    Route::get('faq-list', [FaqController::class, 'index'])->name('faq.list.admin');
+    
+    Route::get('tag-list', [TagsController::class, 'index'])->name('tag.list.admin');
+    
+    Route::get('category-list', [CategoryController::class, 'index'])->name('category.list.admin');
+    
 Route::get('subcategory-list', [SubCategoryController::class, 'index'])->name('subcategory.list.admin');
 
 Route::get('voucher-list', function(){
     return view('pages.admin.voucher.index');
-})->name('voucher.list.admin');
+    })->name('voucher.list.admin');
 
-Route::get('admin-account-list', function(){
-    return view('pages.admin.account.admin');
+    Route::get('admin-account-list', function(){
+        return view('pages.admin.account.admin');
 })->name('admin-account.list.admin');
 
 Route::get('author-list', function(){
@@ -60,9 +52,9 @@ Route::get('author-list', function(){
 
 Route::get('author-banned', function(){ 
     return view('pages.admin.author.author-banned');
-})->name('author-banned.admin');
-
-Route::get('confirm-author-list', function(){
+    })->name('author-banned.admin');
+    
+    Route::get('confirm-author-list', function(){
     return view('pages.admin.author.confirm-author');
 })->name('confirm-author.admin');
 
@@ -77,3 +69,29 @@ Route::get('admin-account-list', function(){
 Route::get('singlepost/news', function(){
     return view('pages.user.singlepost.index');
 })->name('singlepost.news');
+
+Route::get('advertisement-list', function(){
+    return view('pages.admin.advertisement.advertisement-list');
+})->name(('advertisement-list.admin'))
+
+Route::get('confirm-advertisement', function(){
+    return view('pages.admin.advertisement.confirm-advertisement');
+})->name('confirm-advertisement.admin');
+
+Route::get('detail-', function(){
+    return view('pages.admin.advertisement.detail-advertisement');
+})->name('detail-advertisement.admin');
+
+// user
+
+Route::get('news/category', function(){
+    return view('pages.user.category.index');
+})->name('news.category');
+
+Route::get('news/subcategory', function () {
+    return view('pages.user.subcategory.index');
+})->name('news.subcategory');
+
+Route::get('all-news', function(){
+    return view('pages.user.all-news.index');
+})->name(news.all-news);
