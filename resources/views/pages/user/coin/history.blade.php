889c4a0e5f78d6a56f0dd512b9f2e0b347527861
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-06-10 09:25:34 +0700

    berlangganan admin

--- resources/views/pages/user/coin/history.blade.php
+++ resources/views/pages/user/coin/history.blade.php
@@ -36,7 +36,7 @@
         </div>
 
         <div class="">
-            <a href="{{route('user.tukar.coin')}}" class="btn btn-md text-white" style="background-color: #FCD52F;">
+            <a href="{{route('exchange-coin.user')}}" class="btn btn-md text-white" style="background-color: #FCD52F;">
                 Tarik Saldo Sekarang
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                     <path fill="#ffffff" d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z"/>
