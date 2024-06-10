43b01bc38e9bd68f910ab4c6f61363f5af9bc408
ArdianS18 <ardiansupriadi464@gmail.com>
2024-06-10 13:35:17 +0700

    Update sidebar.blade.php

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -99,8 +99,8 @@
                 </li>
 
                 <li class="sidebar-item">
-                    <a class="sidebar-link"
-                        href="" aria-expanded="false">
+                    <a class="sidebar-link {{ request()->routeIs('create.news' ? 'active' : '') }}"
+                        href="{{ route('create.news') }}" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
                         <span class="hide-menu">Upload Artkel</span>
                     </a>
