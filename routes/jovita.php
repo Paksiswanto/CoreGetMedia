e246969ef1b4d619f8adf1fd75de3b7213797dc3
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-06-12 10:38:42 +0700

    categori

--- routes/jovita.php
+++ routes/jovita.php
@@ -1,6 +1,10 @@
 <?php
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NewsCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('news/category', function(){
     return view('pages.user.category.index');
 })->name('news.category');
+
+Route::get('{category}', [NewsCategoryController::class, 'index'])->name('categories.show.user');
