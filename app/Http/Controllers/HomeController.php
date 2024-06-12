4289e1fdbeb2ad8904a3de3d374ceb78cb60a4b8
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-12 09:28:32 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\Interfaces\CategoryInterface;
+use App\Contracts\Interfaces\FaqInterface;
+use App\Contracts\Interfaces\PopularInterface;
+use App\Contracts\Interfaces\SubCategoryInterface;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private FaqInterface $faqs;
+    private CategoryInterface $categories;
+    private SubCategoryInterface $subCategories;
+    private PopularInterface $populars;
+
+    public function __construct(FaqInterface $faqs, CategoryInterface $categories, SubCategoryInterface $subCategories, PopularInterface $populars)
     {
-        $this->middleware('auth');
+        $this->faqs = $faqs;
+        $this->categories = $categories;
+        $this->subCategories = $subCategories;
+        $this->populars = $populars;
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+    public function index(){
+        $categories = $this->categories->get();
+        $subCategories = $this->subCategories->get();
+        $populars = $this->populars->getpopular();
+        return view('pages.index', compact('categories', 'subCategories', 'populars'));
     }
 }
