a0a6c50b7df056080fd4307eefa5be5dfad0bd1e
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-14 00:05:56 +0700

    Update 2023_06_06_122317_create_authors_table.php

--- database/migrations/2023_06_06_122317_create_authors_table.php
+++ database/migrations/2023_06_06_122317_create_authors_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->text('cv');
+            $table->longText('description');
             $table->enum('status', [AuthorEnum::PENDING->value, AuthorEnum::ACCEPTED->value, AuthorEnum::REJECT->value])->default(AuthorEnum::PENDING->value);
             $table->timestamps();
         });
