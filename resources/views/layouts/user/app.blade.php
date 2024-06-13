c5db64d249929a22bd117d00f30f6f2700248faa
Abdul Kader <abdulkader0126@gmail.com>
2024-06-13 09:17:37 +0700

    icon seo

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <meta name="description" content="Sebuah portal berita untuk membaca berita yang saedang trending dan hot">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     @include('layouts.user.css')
     <title>@yield('title', 'Halaman') - {{ config('app.name') }} </title>
-
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('icongetmedia.png') }}">
+    @yield('seo')
     @yield('style')
     <style>
         .theme-dark .tag-list li a {
