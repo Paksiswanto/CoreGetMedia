edd5c23d54a24f261cfd5e2e3cf6af274a931d4b
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-13 22:56:01 +0700

    Update 2023_06_06_122317_create_authors_table.php

--- database/migrations/2023_06_06_122317_create_authors_table.php
+++ database/migrations/2023_06_06_122317_create_authors_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->text('cv');
             $table->enum('status', [AuthorEnum::PENDING->value, AuthorEnum::ACCEPTED->value, AuthorEnum::REJECT->value])->default(AuthorEnum::PENDING->value);
             $table->timestamps();
