12d7939fa55186b680bb276d386bfee3c7fd3383
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-13 09:50:29 +0700

    contact us
    optimize

--- routes/farah.php
+++ routes/farah.php
@@ -1,3 +1,6 @@
 <?php
+
+use App\Http\Controllers\ContactUsController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('contact/store', [ContactUsController::class, 'store'])->name('contact.send');
