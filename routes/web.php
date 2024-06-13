a612946e54bdb87ef450ec2b1db49f0758ab6028
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-14 00:15:21 +0700

    detail author (tunda)
    optimize

--- routes/web.php
+++ routes/web.php
@@ -222,6 +222,7 @@ Route::get('jksaj', [NewsCategoryController::class, 'showAll'])->name('allcatego
 // Route::get('{category}', [NewsController::class, 'showCategories'])->name('categories.show.user'
 
 Route::get('author', [AuthorController::class, 'landing'])->name('user.list.author');
+Route::get('author/{author}', [AuthorController::class, 'show'])->name('author.detail');
 Route::get('news/latest-news', [NewsController::class, 'latestNews'])->name('latest.news');
 Route::get('news/popular-news', [NewsController::class, 'popularNews'])->name('popular.news');
 require_once __DIR__ . '/jovita.php';
